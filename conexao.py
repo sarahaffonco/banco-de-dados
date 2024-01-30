6c70dfd9c2e133e9ccf218f2eaedd357ae94a92d
SarahAffoncom <sarah.affonco@live.com>
2024-01-30 17:56:26 -0300

    inicio exercicios

--- conexao.py
+++ conexao.py
@@ -1,10 +1,23 @@
+## exercicio banco de dados
 import sqlite3
 conexao = sqlite3.connect('banco')
 cursor = conexao.cursor()
 
-#cursor.execute('CREATE TABLE usuarios(id INT, nome VARCHAR(100), endereco VARCHAR(100), email VARCHAR(100))')
 
-cursor.execute('ALTER TABLE usuarios RENAME to usuário')
+# cursor.execute('CREATE TABLE alunos(id INT, nome VARCHAR(100), idade INT, curso VARCHAR(100))')
+
+# cursor.execute('INSERT INTO alunos(id, nome, idade, curso) VALUES (6, "Joao", 30, "fisica")')
+
+#cursor.execute ('DELETE FROM alunos where id=5')
+
+#dados = cursor.execute ('SELECT * FROM alunos')
+#for alunos in dados:
+#    print (alunos)
+
+dados = cursor.execute ('SELECT * FROM alunos WHERE curso = "Engenharia" ORDER BY nome')
+for alunos in dados:
+    print (alunos)
+
 conexao.commit()
 
 
